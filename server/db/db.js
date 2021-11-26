562804108c45472dfe6bad0bf8f35382afb41d89
dagh1 <dalirecardo@gmail.com>
2021-11-27 00:10:58 +0100

    ticket 1 : dev done

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,11 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL ||
+    "postgres://postgres:daghlol10@127.0.0.1:5432/messenger",
+  {
+    logging: false,
+  }
+);
 
 module.exports = db;
